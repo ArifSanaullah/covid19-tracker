8ea8b8008012f4ca42a788beb20b908fa4857208
ArifSanaullah <ak7896023@gmail.com>
2020-06-04 23:55:50 +0500

    feat: Add reset css code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Loader from "./components/Loader";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      rootData: null,
+      countries: [],
+    };
+  }
+
+  setRootData(data) {
+    this.setState((prevState) => ({ ...prevState, rootData: data }));
+  }
+
+  setCouneries(countries) {
+    this.setState((prevState) => ({ ...prevState, countries }));
+  }
+
+  async getSummary() {
+    const { data } = await axios.get("https://api.covid19api.com/summary");
+    const jsonData = JSON.stringify(data, null, "  ");
+    this.setRootData(jsonData);
+  }
+
+  componentDidMount() {
+    try {
+      this.getSummary();
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="test">Hell world</div>
+        <div className="App">
+          {!this.state.rootData && <Loader />}
+          <pre>{this.state.rootData}</pre>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
